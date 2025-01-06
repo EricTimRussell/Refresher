d15350c7ea1c40f3fc4e5b4c10afca0247d7f2fe
Eric Russell <Erictim.Russell@live.com>
2025-01-06 07:46:46 -0700

    card layout and text area

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <h1>Notes</h1>
+  <div class="notes">
+
+    <div class="card has-background-success p-4">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button has-background-primary-light is-success is-outlined">Add Note</button>
+        </div>
+      </div>
+    </div>
+
+    <div v-for="i in 3" class="card">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam repudiandae laborum laboriosam tenetur
+          blanditiis at, excepturi voluptate recusandae, accusantium sapiente optio fuga nemo debitis neque eveniet iste
+          qui. Autem, facere.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+
   </div>
 </template>
 
@@ -8,4 +36,6 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
