725bb1f453a804d03d9a1980b71daaaea0932847
Eric Russell <Erictim.Russell@live.com>
2025-01-27 09:27:14 -0700

    addEditNote comp

---
+++ src/components/Notes/AddEditNote.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card has-background-success p-4">
+    <div class="field">
+      <div class="control">
+        <textarea v-model="newNote" @input="$emit('update:newNote', newNote)" ref="textareaRef" class="textarea"
+          placeholder="Add a new note">
+        </textarea>
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <slot name="buttons" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// import
+import { ref } from "vue"
+
+// props
+const props = defineProps({
+  newNote: {
+    type: String,
+    required: true
+  },
+})
+let newNote = props.newNote
+
+// ref 
+const textareaRef = ref(null)
+
+// emits
+const emit = defineEmits(['update:newNote'])
+
+// functions
+function focusTextarea() {
+  textareaRef.value.focus()
+}
+
+defineExpose({
+  focusTextarea
+})
+</script>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,20 +1,11 @@
 <template>
   <div class="notes">
-
-    <div class="card has-background-success p-4">
-      <div class="field">
-        <div class="control">
-          <textarea ref="newNoteRef" v-model="newNote" class="textarea" placeholder="Add a new note"></textarea>
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button :disabled="!newNote" @click="addNote"
-            class="button has-background-primary-light is-success is-outlined">Add Note</button>
-        </div>
-      </div>
-    </div>
+    <AddEditNote v-model:newNote="newNote" ref="addEditNoteRef">
+      <template #buttons>
+        <button :disabled="!newNote" @click="addNote"
+          class="button has-background-primary-light is-success is-outlined">Add Note</button>
+      </template>
+    </AddEditNote>
 
     <Notes v-for="note in storeNotes.notes" :key="note.id" :note="note" />
 
@@ -25,6 +16,7 @@
 // imports
 import { ref } from "vue";
 import Notes from "@/components/Notes/Notes.vue";
+import AddEditNote from "@/components/Notes/AddEditNote.vue";
 import { useStoreNotes } from "@/stores/storeNotes";
 
 // store
@@ -33,13 +25,13 @@ const storeNotes = useStoreNotes()
 
 // refs
 const newNote = ref('')
-const newNoteRef = ref(null)
+const addEditNoteRef = ref(null)
 
 // Functions
 function addNote() {
   storeNotes.addNote(newNote.value)
   newNote.value = ''
-  newNoteRef.value.focus()
+  addEditNoteRef.value.focusTextarea()
 }
 
 </script>
