327a1301a9c3397685bb1093efc11226af9af6d0
Eric Russell <Erictim.Russell@live.com>
2025-01-27 08:17:05 -0700

    edit note path and view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import NotesView from "@/views/NotesView.vue"
 import StatsView from "@/views/StatsView.vue"
+import EditNoteView from "@/views/EditNoteView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +9,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'notes',
-      component: NotesView,
+      component: NotesView
     },
     {
       path: '/stats',
       name: 'stats',
-      component: StatsView,
+      component: StatsView
+    },
+    {
+      path: '/editNote/:id',
+      name: 'edit-note',
+      component: EditNoteView
     }
   ],
 })
---
+++ src/views/EditNoteView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="edit-notes">
+    <h1>Edit notes with an id of {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
